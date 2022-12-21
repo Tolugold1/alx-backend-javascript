8da6362fe08b6bf16c19d4ee82df2869261af053
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-12-21 07:55:06 +0100

    Add files via upload

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,34 @@
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+    const user = {
+        status: "pending",
+        value: ""
+    };
+
+    const photo = {
+        status: "pending",
+        value: ""
+    };
+
+    try {
+        const resp1 = await signUpUser(firstName, lastName);
+        user.status = "fullfilled";
+        user.value = resp1
+    } catch (err) {
+        user.status = "rejected";
+        user.value = err.toString();
+    }
+
+    try {
+        const resp2 = await uploadPhoto(fileName);
+        photo.status = "fullfilled";
+        photo.value = resp2;
+    } catch (err) {
+        photo.status = "rejected";
+        photo.value = err.toString();
+    }
+
+    return [user, photo];
+}
