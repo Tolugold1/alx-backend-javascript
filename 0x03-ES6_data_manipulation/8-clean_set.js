fddeb07490f7c2f11902357760e56e020fd3e1ee
Tolulope Adeleke <toluadelekesteve@gmail.com>
2023-01-02 22:56:51 +0100

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,6 @@
 export default function cleanSet(set, startString) {
     let restOfString = [];
+    if (!startString || !startString.length) return "";
     set.forEach((e) => {
         if (e.startsWith(startString) === true) {
             restOfString.push(e.slice(startString.length))
