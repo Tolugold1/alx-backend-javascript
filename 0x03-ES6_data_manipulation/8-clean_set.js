88c102482670de986fcdb5760183913ceacd0407
Tolulope Adeleke <toluadelekesteve@gmail.com>
2023-01-02 22:58:56 +0100

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -3,7 +3,7 @@ export default function cleanSet(set, startString) {
     if (!startString || !startString.length) return "";
     set.forEach((e) => {
         if (e.startsWith(startString) === true) {
-            restOfString.push(e.slice(startString.length))
+            restOfString.push(e.slice(startString.length));
         }
     })
     return restOfString.join('-');
