6afbd4ec5bab504e165816d9bd8119d128715616
Tolulope1 <toluadelekesteve@gmail.com>
2023-01-04 14:21:49 +0000

    update

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,8 @@
+interface Teacher {
+    firstName: string,
+    lastName: string,
+    location: string,
+    fullTimeEmployee: true | false,
+    yearsOfExperience?: number,
+    contract?: any
+}
