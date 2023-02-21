5b7ef329137adf3c5054dfe8082e6218fe14884e
Tolugold <toluadelekesteve@gmail.com>
2023-02-21 11:50:00 +0100

    update

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,9 @@
-console.log("Welcome to Holberton School, what is your name?\n");
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
 process.stdin.setEncoding('utf8');
 process.stdin.on("readable", () => {
     let INPUT = process.stdin.read();
     if (INPUT) {
-        console.log(`Your name is: ${INPUT}`);
+        process.stdout.write(`Your name is: ${INPUT}`);
     };
 });
 
