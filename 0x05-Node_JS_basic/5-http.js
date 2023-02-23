79fa7ce62d91c1e96c8da9f47c6b16c066e22e4b
Tolugold <toluadelekesteve@gmail.com>
2023-02-23 08:25:23 +0100

    update

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -7,10 +7,10 @@ const app = http.createServer(async (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   if (req.url === '/') {
-    res.write('Hello Holberton School!')
+    res.write('Hello Holberton School!');
   }
   if (req.url === '/students') {
-    res.write('This is the list of our students\n')
+    res.write('This is the list of our students\n');
     try {
       const data = await countStudents(process.argv[2]);
       res.end(`${data.join('\n')}`);
