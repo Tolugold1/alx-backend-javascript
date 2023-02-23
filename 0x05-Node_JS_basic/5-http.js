7e7905282ade35a4b1287b079de2e2248ba07b8c
Tolugold <toluadelekesteve@gmail.com>
2023-02-23 08:21:09 +0100

    update

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,24 +1,24 @@
-const http = require("http");
+const http = require('http');
+
 const port = 1245;
 
 const app = http.createServer(async (req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    if (req.url === "/") {
-        res.write("Hello Holberton School!")
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  if (req.url === '/') {
+    res.write('Hello Holberton School!')
+  }
+  if (req.url === '/students') {
+    res.write('This is the list of our students\n')
+    try {
+      const data = await countStudents(process.argv[2]);
+      res.end(`${data.join('\n')}`);
+    } catch (error) {
+      res.end(error.message);
     }
-    if (req.url === "/students") {
-        res.write("This is the list of our students\n")
-        try {
-                const data = await countStudents(process.argv[2]);
-                res.end(`${data.join('\n')}`);
-            } catch (error) {
-                res.end(error.message);
-        }
-    }
-    res.end();
-
-})
+  }
+  res.end();
+});
 
 app.listen(port);
 module.exports = app;
