37fa873aa92a0fd8e327b74e6a04d926de4b628f
Tolugold <toluadelekesteve@gmail.com>
2023-02-23 08:14:08 +0100

    update

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,14 +1,11 @@
-const http = require("http");
+const http = require('http');
+
 const port = 1245;
-const host = "localhost"
-
 const app = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json");
-    res.end("Hello Holberton School!")
-})
-
-app.listen(port, host, () => {
-    console.log("listening on port:", port)
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.end('Hello Holberton School!');
 });
+
+app.listen(port);
 module.exports = app;
