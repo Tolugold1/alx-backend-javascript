4548a62d6569b9b46361f18333fcee4e72a52335
Tolugold <toluadelekesteve@gmail.com>
2023-02-24 19:53:17 +0100

    update

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,27 +4,20 @@ const calculateNumber = require("./0-calcul");
 describe('check for rounded values', () => { 
   it("should return the sum of the two argument supplied", function() {
     const a1 = calculateNumber(1.2, 3.7);
-    assert(a1, 5);
-  });
-});
-
-describe('check for rounded values', () => { 
-  it("should return the sum of the two argument supplied", function() {
+    assert.equal(a1, 5);
     const a2 = calculateNumber(1.2, 3);
-    assert(a2, 4);
-  });
-});
-
-describe('check for rounded values', () => { 
-  it("should return the sum of the two argument supplied", function() {
+    assert.equal(a2, 4);
     const a3 = calculateNumber(1, 3.7);
-    assert(a3, 5);
-  });
-});
-
-describe('check for rounded values', () => { 
-  it("should return the sum of the two argument supplied", function() {
+    assert.equal(a3, 5);
     const a4 = calculateNumber(1.5, 3.7);
-    assert(a4, 6);
+    assert.equal(a4, 6);
+  });
+  it("testing for negative numbers", () => {
+    const a1 = calculateNumber(-1, -3);
+    assert.equal(a1, -4);
+    const a2 = calculateNumber(-1.2, -1.1);
+    assert.equal(a2, -2);
+    const a3 = calculateNumber(-1, 1);
+    assert.equal(a3, 0);
   });
 });
