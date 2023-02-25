b5a1a63d40baf9abd2c4405a5775bab990b86b50
Tolugold <toluadelekesteve@gmail.com>
2023-02-25 07:03:54 +0100

    update

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -3,17 +3,28 @@ const calculateNumber = require("./1-calcul.js");
 const chai = require('chai');
 const expect = chai.expect
 
-describe('check for rounded values', () => { 
-  it("should return the sum of the two argument supplied", function() {
+describe('check for type === SUM', () => { 
+  it('should return the sum of the two argument supplied', function() {
     expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+    expect(calculateNumber('SUM', -1, -4)).to.equal(-5); 
+  });
+});
+describe('Check for type === SUBTRACT', () => {
+  it("should subtract two numbers", () => {
     expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+    expect(calculateNumber('SUBTRACT', -1.4, -4.5)).to.equal(3);
+  });
+});
+describe('Check for type === DIVIDE', () => {
+  it("should divide two numbers", () => {
+    expect(calculateNumber('DIVIDE', -1.4, -4.5)).to.equal(0.25);
+    expect(calculateNumber('DIVIDE', -1.4, 0)).to.equal('Error');
     expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
     expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
   });
-  it("testing for negative numbers", () => {
-    expect(calculateNumber('SUM', -1, -4)).to.equal(-5); 
-    expect(calculateNumber('SUBTRACT', -1.4, -4.5)).to.equal(3);
-    expect(calculateNumber('DIVIDE', -1.4, -4.5)).to.equal(0.25);
-    expect(calculateNumber('DIVIDE', -1.4, 0)).to.equal('Error');
-  });
 });
+
+
+
+
+
