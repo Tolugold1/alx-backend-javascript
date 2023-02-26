97f901d6de3bbe322b9feb87d3420c0c1ef06d0b
Tolugold <toluadelekesteve@gmail.com>
2023-02-26 12:29:08 +0100

    update

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,18 +1,18 @@
-const sendPaymentRequestToApi = require('./3-payment');
+const sendPaymentRequestToApi = require('./4-payment.js');
 const utils = require("./utils");
 const sinon = require("sinon");
 const chai = require('chai');
 const expect = chai.expect;
 
 describe("check for sendPaymentRequestToApi", () => {
+  const spies = sinon.spy(console, 'log');
   it("validate the use of utils function", () => {
-    const spies = sinon.spy(console, 'log')
     const stubs = sinon.stub(utils, 'calculateNumber');
-    stubs.calledWith('SUM', 100, 20).returns(10);
+    stubs.withArgs('SUM', 100, 20).returns(10);
     sendPaymentRequestToApi(100, 20);
     expect(spies.calledOnce).to.be.true;
-    expect(stubs.calledWith('The total is: 10')).to.be.true;
-    spies.restore();
+    expect(spies.calledWith('The total is: 10')).to.be.true;
     stubs.restore();
+    spies.restore();
   });
 });
